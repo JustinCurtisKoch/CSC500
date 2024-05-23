60961b373a7d8c921d6f1e7c927d0d0ff1ab4768
JustinCurtisKoch <justincurtiskoch@gmail.com>
2024-05-23 12:14:23 -0400

    assignment commit

---
+++ CSC500-Mod6-DF.py
@@ -0,0 +1,29 @@
+# Creating a list
+Mod6_list = [1, 2, 3, 4, 5]
+
+# Inserting an element
+Mod6_list.append(6)
+print(Mod6_list)
+
+# Updating an element
+Mod6_list[0] = 10
+print(Mod6_list)
+
+# Removing an element
+Mod6_list.remove(3)
+print(Mod6_list)
+
+# Creating a dictionary
+Mod6_dict = {"a": "apple", "b": "banana", "c": "cantaloupe"}
+
+# Inserting an element
+Mod6_dict["d"] = "daikon"
+print(Mod6_dict)
+
+# Updating an element
+Mod6_dict["b"] = "broccoli"
+print(Mod6_dict)
+
+# Removing an element
+Mod6_dict.pop("c")
+print(Mod6_dict)
