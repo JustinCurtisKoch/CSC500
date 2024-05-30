1eb35df345756296a8f169afddb3e4a8a08813d2
JustinCurtisKoch <justincurtiskoch@gmail.com>
2024-05-30 15:55:42 -0400

    commit

---
+++ CSC500-Mod7-CT.py
@@ -0,0 +1,13 @@
+
+# Example of predefined vs user defined to calculate the power of number
+
+# User-defined function to calculate square of a number
+def square(num):
+    print(num * num)
+square(9)
+   
+
+# Predefined function to calculate square of a number
+number = 9
+square = pow(number, 2)
+print(square)
